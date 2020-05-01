b0825a13e51b61ecfb3f7d99219583f9b4e8d130
fansckys <15274027565@163.com>
2020-05-01 19:44:40 +0800

    Initial Commit

---
+++ src/router/router.js
@@ -0,0 +1,21 @@
+// 首个路由为首页
+module.exports = [{
+  path: 'pages/index/index',
+  name: 'Index',
+  config: {
+    navigationBarTitleText: '文章详情'
+  }
+}, {
+  path: 'pages/logs/index',
+  name: 'logs',
+  config: {
+    navigationBarTitleText: 'logs'
+  }
+},
+{
+  path: 'pages/counter/index',
+  name: 'counter',
+  config: {
+    navigationBarTitleText: 'counter'
+  }
+}]
